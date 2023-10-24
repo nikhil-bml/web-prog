119b6c26d28997b53eb28ea382006f2b4579ad18
nikhil-bml <nikhil.tomar.22cse@bmu.edu.in>
2023-10-24 11:53:50 +0530

    Added Automatic resizing

--- src/healthy_hunger/models.py
+++ src/healthy_hunger/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from django_resized import ResizedImageField
 # Create your models here.
 class Product(models.Model):
     name = models.TextField(max_length=300)
     description = models.TextField(max_length=1000)
-    product_image = models.FileField(upload_to="product/")
+    product_image = ResizedImageField(size=[600,400],quality=100,upload_to="product/")
     ingredients = models.TextField(max_length=3000)
-    nutrition_image = models.FileField(upload_to="nutrition/") 
+    nutrition_image = ResizedImageField(size=[700,1350],quality=100,upload_to="nutrition/") 
 
     def __str__(self) -> str:
         return f"{self.id}_{self.name}"
